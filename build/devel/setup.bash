a5b4d860972fcc38f826fc8fefa12ea0c80c21b8
Bastion Fennell <bastionfennell@utexas.edu>
2013-05-01 13:23:39 -0500

    Deleted files that will be built on user computer

--- build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/nishome/bfennell/ros/rosbuild_ws/class-code/visual_front_end/build/devel/setup.sh"
--- build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/nishome/bfennell/ros/rosbuild_ws/class-code/visual_front_end/build/devel/setup.sh"
-emulate zsh # back to zsh mode
